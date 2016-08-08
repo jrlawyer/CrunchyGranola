4ac43e6390b179b35e9126ebd6843ec4510b05c4
Jennifer <jrlawyer@gmail.com>
2016-08-08 16:38:33 -0400

    Initial commit

---
+++ CrunchyGranola/CrunchyGranola/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrunchyGranola.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime DateOfLastPurchase { get; set; }
+
+        public virtual ICollection<Product>Products { get; set; }
+    }
+}
---
+++ CrunchyGranola/CrunchyGranola/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace CrunchyGranola.Models
+{
+    public class Product
+    {
+        public int UpcCode { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+        public string Quantity { get; set; }
+        public string LeadTimes { get; set; }
+
+        public virtual Department Department { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
