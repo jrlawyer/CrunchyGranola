2c68b41c00e4fbfa99d8001ae4275e5417447455
Jennifer <jrlawyer@gmail.com>
2016-08-08 16:56:24 -0400

    I added the database context.

---
+++ CrunchyGranola/CrunchyGranola/DAL/CrunchyGranolaContext.cs
@@ -0,0 +1,28 @@
+﻿using CrunchyGranola.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace CrunchyGranola.DAL
+{
+    public class CrunchyGranolaContext : DbContext
+    {
+        public CrunchyGranolaContext() : base("CrunchyGranolaContext")
+        {
+
+        }
+
+        public DbSet<Customer>Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
