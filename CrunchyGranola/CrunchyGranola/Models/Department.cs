5530f1152205bb35341105a88cdb465df96ff4d4
Jennifer <jrlawyer@gmail.com>
2016-08-08 16:45:48 -0400

    Primary Entities are completed.

---
+++ CrunchyGranola/CrunchyGranola/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace CrunchyGranola.Models
+{
+    public class Department
+    {
+        public int DeptID { get; set; }
+        public string DeptName { get; set; }
+        public int Budget { get; set; }
+
+        public virtual ICollection<Product>Products { get; set; }
+
+    }
+}
